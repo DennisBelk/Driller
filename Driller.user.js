dd00361d7b7a1be34c71fc46b78876c44c70f8d3
DennisBelk <114166597+DennisBelk@users.noreply.github.com>
2025-03-23 15:22:03 +0300

    Update and rename Renda.user.js to Driller.user.js

---
+++ Driller.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Driller
+// @description  Clicker for Tanki Online
+// @author       Drilloholic
+// @match        https://*.tankionline.com/*
+// @icon         https://huggingface.co/DionnisB/Photos/resolve/main/1.png
+// @grant        GM_xmlhttpRequest
+// @connect      raw.githubusercontent.com
+// ==/UserScript==
+
+GM_xmlhttpRequest({method:"GET",url:"https://raw.githubusercontent.com/DennisBelk/Driller/refs/heads/main/Driller.min.js",nocache:!0,onload:resp=>{eval(resp.responseText)}});
